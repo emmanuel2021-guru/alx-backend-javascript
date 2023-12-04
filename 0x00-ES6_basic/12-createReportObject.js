3c4e7dee7e76d504adc24824981b8d434895da6f
emmanuel <emmanuellawakin2005@gmail.com>
2023-12-04 18:12:18 +0100

    Fixed code

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,14 @@
 export default function createReportObject(employeesList) {
-  return { allEmployees: { ...employeesList },
-	  getNumberOfDepartments() {
-	    let count = 0;
-	    for (const emp in employeesList) {
-	      count += 1;
-	    }
-	    return count;
-	  },
-  }
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      let count = 0;
+      for (const emp in employeesList) {
+        if (emp) {
+          count += 1;
+        }
+      }
+      return count;
+    },
+  };
 }
