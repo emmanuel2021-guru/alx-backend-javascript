12b57d16e2b8c7898b8f97044ad5ec72f41d1f89
emmanuel <emmanuellawakin2005@gmail.com>
2023-12-12 19:28:46 +0100

    Completed task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -30,14 +30,26 @@ export default class HolbertonCourse {
   }
 
   set name(value) {
-    this._name = value;
+    if (typeof (value) === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
   }
 
   set length(value) {
-    this._length = value;
+    if (typeof (value) === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 
   set students(value) {
-    this._students = value;
+    if (typeof (students) === 'object') {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an object');
+    }
   }
 }
