6f27041524e33bd6fd34842cc6d7c3f487da0e3d
emmanuel <emmanuellawakin2005@gmail.com>
2023-12-27 01:28:14 +0100

    removed double quotes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (!this.evacuationWarningMessage) {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
