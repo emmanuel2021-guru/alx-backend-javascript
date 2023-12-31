da843157da790bcd18ae73aa3db51856ab3f62f9
emmanuel <emmanuellawakin2005@gmail.com>
2023-12-31 01:56:44 +0100

    Checking

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,9 +5,9 @@ export default class Car {
     this._color = color;
   }
 
-  static get [Symbol.species]() {
+  /*static get [Symbol.species]() {
     return Car;
-  }
+  }*/
 
   cloneCar() {
     return new (this.constructor)();
