d50e48ca135c7199732a2125de832b227f302ec0
emmanuel <emmanuellawakin2005@gmail.com>
2024-01-03 18:23:12 +0100

    Completed task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  let int8View = new DataView(new ArrayBuffer(length));
+  if (position <= int8View.byteLength) {
+    int8View.setInt8(position, value);
+  } else {
+    throw new Error('Position outside range');
+  }
+
+  return int8View;
+}
