cbac420a41fa9cb9f43e899d165e175149be6884
emmanuel <emmanuellawakin2005@gmail.com>
2024-01-08 23:17:34 +0100

    Changed let to const

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if (map instanceof Map) {
-    for (let [key, value] of map) {
+    for (const [key, value] of map) {
       if (value === 1) {
         map.set(key, 100);
       }
