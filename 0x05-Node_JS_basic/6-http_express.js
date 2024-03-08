a743460c635849ec3acff47f61a6f0ad9400b186
emmanuel <emmanuellawakin2005@gmail.com>
2024-03-08 20:14:05 +0100

    Fixed code

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,20 +1,14 @@
-#!/usr/bin/node
-
-/* http server using express
-* @author: Al-Areef
-* */
-
 const express = require('express');
 
 const app = express();
-const port = 1245;
+const PORT = 1245;
 
-app.get('/', (req, res) => {
-  res.status(200);
-  res.end('Hello Holberton School!');
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
+
 module.exports = app;
